540dba1a5c8f9ec2d005f4a7807a4bfe1cf158c7
EqSorting <hydrapandemonium@gmail.com>
2025-03-23 22:09:56 +0700

    Добавлены функции сложения, вычитание и кривое транспонирование

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,3 +1,5 @@
+import math
+
 class Matrix:
       length = 0
       width = 0
@@ -25,9 +27,8 @@ def main():
         print()
 
     print()
+    replace_element(element, Matrix.width, Matrix.length)
     apply_matrix(element, Matrix.width, Matrix.length)
-
-
 
 def apply_matrix(matrix, width, length):
     print("Что хотите сделать?")
@@ -43,13 +44,53 @@ def apply_matrix(matrix, width, length):
     choice = int(input("Ваш выбор:"))
 
     if choice == 1:
-       pass
+       width2 = width;
+       length2 = length;
+       print("Введите элементы матрицы")
+       element2 = [[0 for i in range(length2)] for j in range(width2)]
+       for i in range (width2):
+          for j in range (length2):
+               if element2[i][j] == ' ':
+                  print("Ошибка ввода матрицы")
+                  return main()
+               else:
+                  element2[i][j] = float(input())
+
+       for i in range (width2):
+          for j in range (length2):
+              matrix[i][j] += element2[i][j]
+
+       for i in range (width):
+          for j in range (length):
+              print(f"{matrix[i][j]} ", end="")
+          print()
+       return apply_matrix(matrix, width, length)
     elif choice == 2:
-       pass
+       width3 = width;
+       length3 = length;
+       print("Введите элементы матрицы")
+       element3 = [[0 for i in range(length3)] for j in range(width3)]
+       for i in range (width3):
+          for j in range (length3):
+               if element3[i][j] == ' ':
+                  print("Ошибка ввода матрицы")
+                  return main()
+               else:
+                  element3[i][j] = float(input())
+
+       for i in range (width3):
+          for j in range (length3):
+              matrix[i][j] -= element3[i][j]
+
+       for i in range (width):
+          for j in range (length):
+              print(f"{matrix[i][j]} ", end="")
+          print()
+       return apply_matrix(matrix, width, length)
     elif choice == 3:
        target = float(input("Введи число, на которое хотите умножить матрицу"))
-       for i in range(Matrix.width):
-          for j in range(Matrix.length):
+       for i in range(width):
+          for j in range(length):
               matrix[i][j] = target * matrix[i][j]
 
        for i in range(width):
@@ -68,16 +109,53 @@ def apply_matrix(matrix, width, length):
           for j in range(length):
               matrix[i][j] = matrix[i][j] / target2
 
-       for i in range(Matrix.width):
-          for j in range(Matrix.length):
+       for i in range(width):
+          for j in range(length):
               print(f"{matrix[i][j]} ", end="")
           print()
        return apply_matrix(matrix, width, length)
     elif choice == 6:
-       pass
+       width = length
+       length = width
+       elements = [[0 for i in range(length)] for j in range(width)]
+       for i in range(length):
+          for j in range(width):
+               if i < len(matrix) and j < len(matrix[0]):
+                   elements[j][i] = matrix[i][j]
+
+       matrix = elements
+
+       for i in range(length):
+          for j in range(width):
+                   print(f"{matrix[i][j]} ", end="")
+          print()
     elif choice == 7:
        pass
     elif choice == 8:
        return 0
 
+def replace_element(matrix, width, length):
+    flag = 1
+    while flag == 1:
+        char = input("Хотите заменить элемент?")
+        if char == 'Да':
+            replace_width = int(input("Введите строку, где хотите заменить элемент"))
+            replace_length = int(input("Введите столбец, где хотите заменить элемент"))
+            value = float(input("На какое значение хотите поменять?"))
+            for i in range(width):
+                for j in range(length):
+                    if matrix[i][j] == matrix[replace_width][replace_length]:
+                       matrix[replace_width][replace_length] = value
+
+            for i in range(width):
+                for j in range(length):
+                      print(f"{matrix[i][j]} ", end="")
+                print()
+            flag = 1
+        else:
+            flag = 0
+            print("Подождитe...")
+
 __name__ = "__main__" and main()
+
+
